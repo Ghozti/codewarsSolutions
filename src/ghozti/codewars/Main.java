d8f8943dccb5fab7ff2879eba2c79cc487fb20a3
Ghozti <ybalderas1005@gmail.com>
2021-02-19 09:52:52 -0500

    solved and commented Dubstep

--- src/ghozti/codewars/Main.java
+++ src/ghozti/codewars/Main.java
@@ -1,13 +1,8 @@
 package ghozti.codewars;
 
-import ghozti.codewars.solutions.kyu6.StringSplit;
-
 public class Main {
 
     public static void main(String[] args) {
-        String[] arr = StringSplit.solution("dogandcat");
-        for (String i : arr){
-            System.out.println(i);
-        }
+
     }
 }
--- src/ghozti/codewars/solutions/kyu6/Dubstep.java
+++ src/ghozti/codewars/solutions/kyu6/Dubstep.java
@@ -1,7 +1,31 @@
 package ghozti.codewars.solutions.kyu6;
 
+import java.util.ArrayList;
+
 public class Dubstep {
+
+    final int KYU = 6;
+
+    /**
+     *THIS METHOD:
+     * will take a string with WUB in between characters such as "STARWUBWUBWUBWUBWUBWARSWUB"
+     * then it will replace all WUB with " " so it ends up being "STAR     WARS "
+     * then it trims the string so it's "STAR     WARS"
+     * then it replaces all " " with "1" so it's "STAR11111WARS"
+     * then it replaces all "11" with "1" so it's "STAR111WARS" (does this twice)
+     * then it replaces all "1" with " " so it's "STAR  WARS"
+     * then the while loop will repetitively do this until the string has no extra white spaces. (without the while loop the string would end up being "STAR  WARS" and not "STAR WARS"
+     */
+
     public static String SongDecoder (String song){
-        return song.replaceAll("WUB","");
+        String ssong;
+        song = song.replaceAll("WUB"," ").trim().replaceAll(" ","1").replaceAll("11","1").replaceAll("11","1").replaceAll("1"," ");
+        while (true){
+            ssong = song.replaceAll("WUB"," ").trim().replaceAll(" ","1").replaceAll("11","1").replaceAll("11","1").replaceAll("1"," ");
+            if (!ssong.equals(song)){
+                break;
+            }
+        }
+        return ssong;
     }
 }
