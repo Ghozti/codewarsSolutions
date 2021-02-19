ab860698f20dc272ade47d5db425683c7a2d7ebe
Ghozti <ybalderas1005@gmail.com>
2021-02-19 15:02:00 -0500

    added and started to work on EnoughIsEnough

--- src/ghozti/codewars/Main.java
+++ src/ghozti/codewars/Main.java
@@ -1,8 +1,12 @@
 package ghozti.codewars;
 
+import ghozti.codewars.solutions.kyu6.EnoughIsEnough;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        for (int i = 0; i < EnoughIsEnough.deleteNth( new int[] { 20, 37, 20, 21 }, 1 ).length; i++) {
+            System.out.println(EnoughIsEnough.deleteNth( new int[] { 20, 37, 20, 21 }, 1 )[i]);
+        }
     }
 }
---
+++ src/ghozti/codewars/solutions/kyu6/EnoughIsEnough.java
@@ -0,0 +1,20 @@
+package ghozti.codewars.solutions.kyu6;
+
+import java.util.ArrayList;
+
+public class EnoughIsEnough {
+
+    public static int[] deleteNth(int[] elements, int maxOccurrences) {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < elements.length; i++) {
+            arr.add(elements[i]);
+        }
+
+        for (int i = 0; i < arr.size(); i++) {
+
+        }
+        return elements;
+    }
+
+}
