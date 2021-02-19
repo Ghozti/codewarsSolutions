7344cb251a47d26c903235714358f11d2846db12
Ghozti <ybalderas1005@gmail.com>
2021-02-19 18:11:04 -0500

    created FindTheUniqueNumber

--- src/ghozti/codewars/Main.java
+++ src/ghozti/codewars/Main.java
@@ -1,12 +1,9 @@
 package ghozti.codewars;
 
-import ghozti.codewars.solutions.kyu6.EnoughIsEnough;
 
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < EnoughIsEnough.deleteNth((new int[] { 1, 2, 3, 1, 1, 2, 1, 2, 3, 3, 2, 4, 5, 3, 1 }), 3).length; i++) {
-            System.out.println(EnoughIsEnough.deleteNth((new int[] { 1, 2, 3, 1, 1, 2, 1, 2, 3, 3, 2, 4, 5, 3, 1 }), 3)[i]);
-        }
+
     }
 }
