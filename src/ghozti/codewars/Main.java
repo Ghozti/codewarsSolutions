053d472fc1cb8779f1317f8a69ced020165ec923
Ghozti <ybalderas1005@gmail.com>
2021-02-19 18:05:04 -0500

    deleted enough is enough due to lack of understandment

--- src/ghozti/codewars/Main.java
+++ src/ghozti/codewars/Main.java
@@ -5,8 +5,8 @@ import ghozti.codewars.solutions.kyu6.EnoughIsEnough;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < EnoughIsEnough.deleteNth( new int[] { 20, 37, 20, 21 }, 1 ).length; i++) {
-            System.out.println(EnoughIsEnough.deleteNth( new int[] { 20, 37, 20, 21 }, 1 )[i]);
+        for (int i = 0; i < EnoughIsEnough.deleteNth((new int[] { 1, 2, 3, 1, 1, 2, 1, 2, 3, 3, 2, 4, 5, 3, 1 }), 3).length; i++) {
+            System.out.println(EnoughIsEnough.deleteNth((new int[] { 1, 2, 3, 1, 1, 2, 1, 2, 3, 3, 2, 4, 5, 3, 1 }), 3)[i]);
         }
     }
 }
