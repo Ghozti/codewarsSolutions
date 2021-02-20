fafee0bb5f5bed3d535882cfbd70e41b92faed0b
Ghozti <ybalderas1005@gmail.com>
2021-02-20 15:36:57 -0500

    solved and finished YourOderPlease

--- src/ghozti/codewars/Main.java
+++ src/ghozti/codewars/Main.java
@@ -1,11 +1,10 @@
 package ghozti.codewars;
 
-
-import ghozti.codewars.solutions.kyu6.FindTheUniqueNumber;
+import ghozti.codewars.solutions.kyu6.YourOderPlease;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(FindTheUniqueNumber.findUniq(new double[]{0, 1, 0}));
+
     }
 }
--- src/ghozti/codewars/solutions/kyu6/YourOderPlease.java
+++ src/ghozti/codewars/solutions/kyu6/YourOderPlease.java
@@ -1,4 +1,27 @@
 package ghozti.codewars.solutions.kyu6;
 
+import java.util.ArrayList;
+
 public class YourOderPlease {
+    public static String order(String words){
+        String[] arr = words.split("\\W+");
+        ArrayList<String> sortedString = new ArrayList<>();
+        StringBuilder fString = new StringBuilder();
+
+        int current = 1;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j].contains(current + "")){
+                    sortedString.add(arr[j]);
+                    current++;
+                }
+            }
+        }
+
+        for (int i = 0; i < sortedString.size(); i++) {
+            fString.append(sortedString.get(i) + " ");
+        }
+        return fString.toString().trim();
+    }
 }
