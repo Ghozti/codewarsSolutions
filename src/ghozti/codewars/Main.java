7535ebd8731409d0353576d5dace2c8d49772dba
Ghozti <ybalderas1005@gmail.com>
2021-02-19 19:18:53 -0500

    finished, solved and commented on FindTheUniqueNumber a kyu6

--- src/ghozti/codewars/Main.java
+++ src/ghozti/codewars/Main.java
@@ -1,9 +1,11 @@
 package ghozti.codewars;
 
 
+import ghozti.codewars.solutions.kyu6.FindTheUniqueNumber;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(FindTheUniqueNumber.findUniq(new double[]{0, 1, 0}));
     }
 }
--- src/ghozti/codewars/solutions/kyu6/FindTheUniqueNumber.java
+++ src/ghozti/codewars/solutions/kyu6/FindTheUniqueNumber.java
@@ -1,4 +1,32 @@
 package ghozti.codewars.solutions.kyu6;
 
 public class FindTheUniqueNumber {
+
+    final int KYU = 6;
+
+    /**
+     * THIS METHOD:
+     * will create a compare number variable which will be the non unique number
+     * loop through the doubles in the array and compare the number at index i-1 and i
+     * if they are the same then the compare variable will be assigned this value
+     * then the second loop will loop through the numbers again and compare the index at i with the compare variable
+     * if the number does not equal to "compare" then it will return that number
+     */
+
+    public static double findUniq(double arr[]) {
+        double compare = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i-1] == arr[i]){
+                compare = arr[i-1];
+                break;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != compare){
+                return arr[i];
+            }
+        }
+        return 0;
+    }
 }
