8e17af150a3fc5f3541133e9bf141543641e582a
Ghozti <ybalderas1005@gmail.com>
2021-02-20 15:44:26 -0500

    commented YourOderPlease

--- src/ghozti/codewars/Main.java
+++ src/ghozti/codewars/Main.java
@@ -5,6 +5,6 @@ import ghozti.codewars.solutions.kyu6.YourOderPlease;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(YourOderPlease.order("is2 Thi1s T4est 3a no5"));
     }
 }
--- src/ghozti/codewars/solutions/kyu6/YourOderPlease.java
+++ src/ghozti/codewars/solutions/kyu6/YourOderPlease.java
@@ -3,6 +3,25 @@ package ghozti.codewars.solutions.kyu6;
 import java.util.ArrayList;
 
 public class YourOderPlease {
+
+    final int KYU = 6;
+
+    /**
+     * This method-
+     * will sort the string "words" into an array. (arr)
+     * then create an array list called sortedString
+     * then create a string called fString (final string)
+     * then create an int called current which will be the current number we will look for in a string
+     * the first for loop:
+     * - will execute the for loop inside the for loop as many times as the arr's size is
+     * - check the current value of the string at arr[i] then check if the current string at [i] contains the "current" variable value in a string (ex: if the string at that value is word1 and the current variable holds a value of 1)
+     * - add that string to the sortedString array list
+     * - increment the current variable value by 1
+     *
+     * the next for loop will:
+     * add up the array list variables to fString
+     */
+
     public static String order(String words){
         String[] arr = words.split("\\W+");
         ArrayList<String> sortedString = new ArrayList<>();
