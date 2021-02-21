a1c1cc4503311180dded6cfe619b841f46a2cec2
Ghozti <ybalderas1005@gmail.com>
2021-02-21 10:30:55 -0500

    commented PersistentBugger

--- src/ghozti/codewars/Main.java
+++ src/ghozti/codewars/Main.java
@@ -1,11 +1,8 @@
 package ghozti.codewars;
 
-import ghozti.codewars.solutions.kyu6.PersistentBugger;
-import ghozti.codewars.solutions.kyu6.YourOderPlease;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(PersistentBugger.persistence(999));
+        //call the static methods here
     }
 }
--- src/ghozti/codewars/solutions/kyu6/PersistentBugger.java
+++ src/ghozti/codewars/solutions/kyu6/PersistentBugger.java
@@ -6,6 +6,20 @@ public class PersistentBugger {
 
     final int KYU = 6;
 
+    /**
+     * THIS METHOD:
+     * create a variable called x (for x times that it takes to make the long into a single number digit)
+     * create a while loop who's condition is to run the code below while (n) is greater than 9 (since 9 is the highest single digit number possible)
+     * turn (n) into a string (so we can get each individual number later)
+     * create an array list (arr) which will hold the individual numbers
+     * add the individual numbers of (n) to (arr)
+     * create a long variable (product) which will have an initial value of whatever number arr has first
+     * multiply each individual number with each other
+     * increment x by 1
+     * update (n) to be the value of (product)
+     * return x
+     */
+
     public static int persistence(long n) {
         int x = 0;
         while (n > 9){
