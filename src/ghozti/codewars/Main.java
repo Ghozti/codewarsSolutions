c79da9c06d1ca9265fe9eb192948c1db1820165c
Ghozti <ybalderas1005@gmail.com>
2021-02-21 14:25:25 -0500

    finished FindTheMissingLetter

--- src/ghozti/codewars/Main.java
+++ src/ghozti/codewars/Main.java
@@ -1,8 +1,10 @@
 package ghozti.codewars;
 
+import ghozti.codewars.solutions.kyu6.FindTheMissingLetter;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(FindTheMissingLetter.findMissingLetter(new char[] { 'a','b','c','d','f' }));
     }
 }
--- src/ghozti/codewars/solutions/kyu6/FindTheMissingLetter.java
+++ src/ghozti/codewars/solutions/kyu6/FindTheMissingLetter.java
@@ -1,4 +1,58 @@
 package ghozti.codewars.solutions.kyu6;
 
+import java.util.ArrayList;
+
 public class FindTheMissingLetter {
+
+    public static char findMissingLetter(char[] arr){
+        ArrayList<Character> alphabet = new ArrayList<>();
+        ArrayList<Character> compareArr =  new ArrayList<>();
+        ArrayList<Character> alp = new ArrayList<>();
+
+        boolean upper = false;
+
+        if (arr[0] > 64 && arr[0] < 91) {
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = Character.toLowerCase(arr[i]);
+                upper = true;
+            }
+        }
+
+        for (int i = 97; i < 172; i++) {
+            alphabet.add((char) i);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            compareArr.add(arr[i]);
+        }
+        int sind = 0;
+        int eind = 0;
+
+        for (int j = 0; j < alphabet.size(); j++) {
+            if (alphabet.get(j) == compareArr.get(0)){
+                sind = j;
+            }
+        }
+
+        for (int i = 0; i < alphabet.size()-1; i++) {
+            if (alphabet.get(i) == compareArr.get(compareArr.size()-1)){
+                eind = i;
+            }
+        }
+
+        for (int i = sind; i < eind; i++) {
+            alp.add(alphabet.get(i));
+        }
+
+
+        for (int i = 0; i < compareArr.size(); i++) {
+            if (!compareArr.contains(alp.get(i))){
+                if (upper){
+                    return (alp.get(i) + "").toUpperCase().charAt(0);
+                }
+                return alp.get(i);
+            }
+        }
+        return ' ';
+    }
 }
