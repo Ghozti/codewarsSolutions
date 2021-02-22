6648c633fa528bd675fdd3c4136675fd2bdfebe1
Ghozti <ybalderas1005@gmail.com>
2021-02-22 09:39:13 -0500

    finished and solved DigitalRoots

--- src/ghozti/codewars/Main.java
+++ src/ghozti/codewars/Main.java
@@ -1,10 +1,11 @@
 package ghozti.codewars;
 
+import ghozti.codewars.solutions.kyu6.DigitalRoots;
 import ghozti.codewars.solutions.kyu6.FindTheMissingLetter;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(FindTheMissingLetter.findMissingLetter(new char[] { 'a','b','c','d','f' }));
+        System.out.println(DigitalRoots.digital_root(493193));
     }
 }
--- src/ghozti/codewars/solutions/kyu6/DigitalRoots.java
+++ src/ghozti/codewars/solutions/kyu6/DigitalRoots.java
@@ -1,4 +1,30 @@
 package ghozti.codewars.solutions.kyu6;
 
+import java.util.ArrayList;
+
 public class DigitalRoots {
+    public static int digital_root(int n){
+        if(n <= 9){
+            return n;
+        }
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        String s = n + "";
+
+        for (int i = 0; i < s.length(); i++) {
+            arr.add(Integer.parseInt(s.charAt(i) + ""));
+        }
+
+        int sum = arr.get(0);
+
+        for (int i = 1; i < arr.size(); i++) {
+            sum += arr.get(i);
+        }
+
+        if (sum > 9){
+            sum = digital_root(sum);
+        }
+        return sum;
+    }
 }
