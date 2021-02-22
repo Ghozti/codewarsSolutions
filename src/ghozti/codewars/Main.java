4abdda3c4da84c16c4b897cc79b2ef4fdbd88f11
Ghozti <ybalderas1005@gmail.com>
2021-02-22 10:31:24 -0500

    commented on CreditCardMask

--- src/ghozti/codewars/Main.java
+++ src/ghozti/codewars/Main.java
@@ -1,7 +1,6 @@
 package ghozti.codewars;
 
 public class Main {
-
     public static void main(String[] args) {
 
     }
--- src/ghozti/codewars/solutions/kyu7/CreditCardMask.java
+++ src/ghozti/codewars/solutions/kyu7/CreditCardMask.java
@@ -4,6 +4,16 @@ public class CreditCardMask {
 
     final int KYU = 7;
 
+    /**
+     * THIS METHOD:
+     * checks to see if the string's length is greater than 4 (since we need to mask all but 4 of the characters)
+     * creates 2 strings (masked and non masked)
+     * loops through the string except the last 4 characters
+     * and appends (masked) with '#'
+     * then creates a substring starting from the length of str -4 to it's length (so we get the last 4 characters)
+     * then updates (str) to be masked + nonMasked
+     */
+
     public static String maskify(String str){
         if (str.length() > 4){
             String nonMasked;
