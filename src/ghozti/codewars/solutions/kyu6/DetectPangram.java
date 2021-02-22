3e3078411102e27beeb8d245d39498d774245bf0
Ghozti <ybalderas1005@gmail.com>
2021-02-22 12:53:29 -0500

    finalized and commented on DetectPangram

--- src/ghozti/codewars/solutions/kyu6/DetectPangram.java
+++ src/ghozti/codewars/solutions/kyu6/DetectPangram.java
@@ -3,6 +3,19 @@ package ghozti.codewars.solutions.kyu6;
 import java.util.ArrayList;
 
 public class DetectPangram {
+
+    final int KYU = 6;
+
+    /**
+     * THIS METHOD:
+     * will make (sentence) into a lower case string
+     * create 1 array list and an array. (arr will have each letter in the alphabet) (contains will hold a string value to compare later) (more on this later)
+     * the 2 for loops set values to the arrays (the second one will set "0" for each element in the array which will be used to check if this letter was detected in (sentence)
+     * the other for loop will loop through the (sentence) string and check if it contains a letter from the alphabet if so it will replace that index  value with "1" in the array
+     * ex: if the sentence contains 'a' it will replace the value of the array at index 0 with "1"
+     * the final loop will loop through the array and check to see if every value is equals to "1" (if all 26 values are equal to "1" it means that all 26 letters have been detected)
+     */
+
     public static boolean check(String sentence){
         sentence = sentence.toLowerCase();
 
@@ -25,10 +38,6 @@ public class DetectPangram {
                     break;
                 }
             }
-        }
-
-        for (int i = 0; i < arr.size(); i++) {
-            System.out.println(contains[i]);
         }
 
         for (int i = 0; i < contains.length; i++) {
