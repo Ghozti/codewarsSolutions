b363e029aaa883ab229ff7ccf6fb38ccd922be2a
Ghozti <ybalderas1005@gmail.com>
2021-02-22 10:09:36 -0500

    commented on StopgninnipSMysdroW

--- src/ghozti/codewars/solutions/kyu6/StopgninnipSMysdroW.java
+++ src/ghozti/codewars/solutions/kyu6/StopgninnipSMysdroW.java
@@ -1,6 +1,17 @@
 package ghozti.codewars.solutions.kyu6;
 
 public class StopgninnipSMysdroW {
+
+    final int KYU = 6;
+
+    /**
+     * THIS METHOD:
+     * will create an array of each separate word in (sentence)
+     * then loop through the array and check if the element has a length greater or equal to five
+     * if so it will reverse the word and replace the element with the reversed word
+     * then it will make a string out of the elements out of the array and return that string
+     */
+
     public static String spinWords(String sentence) {
         String[] arr = sentence.split("\\W+");
 
