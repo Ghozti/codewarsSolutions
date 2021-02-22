ea031613d715e5c51924a3004ffaeba59348551e
Ghozti <ybalderas1005@gmail.com>
2021-02-22 16:28:50 -0500

    added SudokuValidator

---
+++ src/ghozti/codewars/solutions/kyu4/SudokuValidator.java
@@ -0,0 +1,9 @@
+package ghozti.codewars.solutions.kyu4;
+
+public class SudokuValidator {
+    public static boolean check(int[][] sudoku) {
+        System.out.println(sudoku[0][1]);
+        //9 subsquares
+        return false;
+    }
+}
