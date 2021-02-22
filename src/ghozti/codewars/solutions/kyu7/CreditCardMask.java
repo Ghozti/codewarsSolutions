ea779b0ff9ec21e480e34768b446d11f41bf147e
Ghozti <ybalderas1005@gmail.com>
2021-02-22 10:22:11 -0500

    solved CreditCardMask

--- src/ghozti/codewars/solutions/kyu7/CreditCardMask.java
+++ src/ghozti/codewars/solutions/kyu7/CreditCardMask.java
@@ -1,4 +1,21 @@
 package ghozti.codewars.solutions.kyu7;
 
 public class CreditCardMask {
+
+    final int KYU = 7;
+
+    public static String maskify(String str){
+        if (str.length() > 4){
+            String nonMasked;
+            StringBuilder masked = new StringBuilder();
+
+            for (int i = 0; i < str.length() - 4; i++) {
+                masked.append('#');
+            }
+
+            nonMasked = str.substring(str.length()-4);
+            str = masked + nonMasked;
+        }
+        return str;
+    }
 }
