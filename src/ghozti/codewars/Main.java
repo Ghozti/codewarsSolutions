8e06a8b2329ed2d86fcfab6b535c0c02349869eb
Ghozti <ybalderas1005@gmail.com>
2021-02-22 10:06:30 -0500

    finished and solved StopgninnipSMysdroW

--- src/ghozti/codewars/Main.java
+++ src/ghozti/codewars/Main.java
@@ -1,8 +1,10 @@
 package ghozti.codewars;
 
+import ghozti.codewars.solutions.kyu6.StopgninnipSMysdroW;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(StopgninnipSMysdroW.spinWords(""));
     }
 }
--- src/ghozti/codewars/solutions/kyu6/StopgninnipSMysdroW.java
+++ src/ghozti/codewars/solutions/kyu6/StopgninnipSMysdroW.java
@@ -1,4 +1,25 @@
 package ghozti.codewars.solutions.kyu6;
 
 public class StopgninnipSMysdroW {
+    public static String spinWords(String sentence) {
+        String[] arr = sentence.split("\\W+");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length() >= 5){
+                String newWord = "";
+                for (int j = arr[i].length()-1; j != -1 ; j--) {
+                    newWord += arr[i].charAt(j);
+                }
+                arr[i] = newWord;
+            }
+        }
+
+        sentence = "";
+
+        for (String i : arr){
+            sentence += i + " ";
+        }
+
+        return sentence.trim();
+    }
 }
