d3ee1cd2fbcc3d0b3ae9added8444387ad658478
Ghozti <ybalderas1005@gmail.com>
2021-02-22 09:46:53 -0500

    commented on DigitalRoots

--- src/ghozti/codewars/Main.java
+++ src/ghozti/codewars/Main.java
@@ -1,11 +1,8 @@
 package ghozti.codewars;
 
-import ghozti.codewars.solutions.kyu6.DigitalRoots;
-import ghozti.codewars.solutions.kyu6.FindTheMissingLetter;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(DigitalRoots.digital_root(493193));
+
     }
 }
--- src/ghozti/codewars/solutions/kyu6/DigitalRoots.java
+++ src/ghozti/codewars/solutions/kyu6/DigitalRoots.java
@@ -3,6 +3,21 @@ package ghozti.codewars.solutions.kyu6;
 import java.util.ArrayList;
 
 public class DigitalRoots {
+
+    final int KYU = 6;
+
+    /**
+     * THIS METHOD:
+     * checks to see if (n) is less than 9 (since 9 is the greatest single digit number we can have) if so it will simply return that number
+     * if the number is not less than 9:
+     * it creates an array list which will hold each individual number ex (n = 999) arr will have {9,9,9}
+     * it converts the int to a string (so we get each individual number)
+     * converts each string index back to a single int and adds it to the arr list
+     * adds each element in the array list with each other
+     * checks if the sum is less than or equal to 9 if so it returns the sum
+     * else the method will call itself to repeat this process as many times as needed
+     */
+
     public static int digital_root(int n){
         if(n <= 9){
             return n;
