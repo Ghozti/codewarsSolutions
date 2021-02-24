f8666f8310943ad37344d33b8f451ef06b635f19
Ghozti <ybalderas1005@gmail.com>
2021-02-24 10:59:51 -0500

    added NumbersInExpandedForm

---
+++ src/ghozti/codewars/solutions/kyu6/NumbersInExpandedForm.java
@@ -0,0 +1,9 @@
+package ghozti.codewars.solutions.kyu6;
+
+public class NumbersInExpandedForm {
+    public static String expandedForm(int num) {
+        for (int i = 0; i < ; i++) {
+            
+        }
+    }
+}
