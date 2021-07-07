d235a841cc7c14daf92cd5ea9e9d5374cc817eef
Ghozti <ybalderas1005@gmail.com>
2021-07-07 00:47:27 -0400

    - worked on EscapeTheMaze.java

--- src/ghozti/codewars/Main.java
+++ src/ghozti/codewars/Main.java
@@ -1,10 +1,21 @@
 package ghozti.codewars;
 
+import ghozti.codewars.solutions.kyu4.EscapeTheMaze;
 import ghozti.codewars.solutions.kyu5.DidYouMean;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        DidYouMean didYouMean = new DidYouMean(new String[]{"cherry", "pineapple", "melon", "strawberry", "raspberry"});
-        System.out.println(didYouMean.findMostSimilar("berry"));
+        EscapeTheMaze.escape(new char[][] {
+                "# #".toCharArray(),
+                " > ".toCharArray(),
+                "# #".toCharArray()
+        });
+        System.out.println(Arrays.deepToString(new char[][]{
+                "# #".toCharArray(),
+                " > ".toCharArray(),
+                "# #".toCharArray()
+        }));
     }
 }
--- src/ghozti/codewars/solutions/kyu4/EscapeTheMaze.java
+++ src/ghozti/codewars/solutions/kyu4/EscapeTheMaze.java
@@ -1,4 +1,17 @@
 package ghozti.codewars.solutions.kyu4;
 
+import java.util.List;
+
 public class EscapeTheMaze {
+
+    public static List<Character> escape(char[][] maze) {
+        for( int i = 0; i < maze.length-1; i++){//number of arrays in maze 2d array
+            System.out.println('*');
+            for(int ii = 0; i < maze[i].length-1; i++) {//current array in maze length
+                System.out.println(maze[i][ii]);
+            }
+        }
+        System.out.println("done");
+        return null;
+    }
 }
