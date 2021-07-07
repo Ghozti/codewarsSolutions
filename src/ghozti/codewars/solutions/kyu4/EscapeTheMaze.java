b062a9a026f64b36576e7106cbd6d4343f4d8316
Ghozti <ybalderas1005@gmail.com>
2021-07-07 01:07:41 -0400

    - added constants

--- src/ghozti/codewars/solutions/kyu4/EscapeTheMaze.java
+++ src/ghozti/codewars/solutions/kyu4/EscapeTheMaze.java
@@ -5,6 +5,16 @@ import java.util.List;
 
 public class EscapeTheMaze {
 
+    //vals on a 2d coordinate plane (x,y)]\
+
+    static int move_forward_facing_right = 1;                   //+1 on the x axis
+
+    static int move_forward_facing_left = -1;                  //-1 on the x axis
+
+    static int move_forward_facing_up = 1;                     //1 on the y axis
+
+    static int getMove_forward_facing_down = - 1;              //-1 on the y axis
+
     public static List<Character> escape(char[][] maze) {
         ArrayList<String> charlist = new ArrayList<>();
             for (int i = 0; i < maze.length; i++) {//number of arrays in maze 2d array
