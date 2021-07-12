93ac0c4d538555fd3e62489c8212ad1685c028c1
Ghozti <ybalderas1005@gmail.com>
2021-07-12 02:54:54 -0400

    - finished week1

--- src/ghozti/codewars/Main.java
+++ src/ghozti/codewars/Main.java
@@ -2,12 +2,13 @@ package ghozti.codewars;
 
 import ghozti.codewars.solutions.kyu4.EscapeTheMaze;
 import ghozti.codewars.solutions.kyu5.DidYouMean;
+import ghozti.codewars.solutions.kyu5.TalkLikeSiegfried;
 import ghozti.codewars.solutions.kyu5.ZooDisaster;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(ZooDisaster.whoEatsWho("fox,bug,chicken,grass,sheep")));
+        System.out.println(TalkLikeSiegfried.siegfried(1,"lolcic"));
     }
 }
--- src/ghozti/codewars/solutions/kyu5/TalkLikeSiegfried.java
+++ src/ghozti/codewars/solutions/kyu5/TalkLikeSiegfried.java
@@ -1,4 +1,61 @@
 package ghozti.codewars.solutions.kyu5;
 
+import java.util.Arrays;
+
 public class TalkLikeSiegfried {
+
+    public static String siegfried(final int week, final String str) {
+        String output = "";
+        return week1(str);
+    }
+
+    private static String week1(String str){
+        String output = "";
+        char[] string_chars = str.toCharArray();
+
+        for(int i = 0; i < string_chars.length; i++){
+            if(i  < str.length()-1){
+                if(string_chars[i] == 'c'){
+                    if(string_chars[i+1] == 'i' || string_chars[i+1] == 'I' || string_chars[i+1] == 'e'|| string_chars[i+1] == 'E'){
+                        string_chars[i] = 's';
+                    }
+                }else if(string_chars[i] == 'C'){
+                    if(string_chars[i+1] == 'i' || string_chars[i+1] == 'I' || string_chars[i+1] == 'e'|| string_chars[i+1] == 'E'){
+                        string_chars[i] = 'S';
+                    }
+                }else if(string_chars[i] == 'c' && string_chars[i+1] != 'h' && string_chars[i+1] != 'H'){
+                        string_chars[i] = 'k';
+                }else if(string_chars[i] == 'C' && string_chars[i+1] != 'h' && string_chars[i+1] != 'H'){
+                        string_chars[i] = 'K';
+                }
+            }else {
+                if(string_chars[i] == 'c'){
+                    string_chars[i] = 'k';
+                }else if(string_chars[i] == 'C'){
+                    string_chars[i] = 'K';
+                }
+            }
+        }
+        return Arrays.toString(string_chars);
+    }
+
+    private static String week2(String str){
+        String output = "";
+        return output;
+    }
+
+    private static String week3(String str){
+        String output = "";
+        return output;
+    }
+
+    private static String week4(String str){
+        String output = "";
+        return output;
+    }
+
+    private static String week5(String str){
+        String output = "";
+        return output;
+    }
 }
