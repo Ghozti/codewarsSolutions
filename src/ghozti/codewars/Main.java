234b30d564745860a7d3620015ffa59a390f2e58
Ghozti <ybalderas1005@gmail.com>
2021-07-12 03:03:46 -0400

    - finished week1

--- src/ghozti/codewars/Main.java
+++ src/ghozti/codewars/Main.java
@@ -9,6 +9,11 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(TalkLikeSiegfried.siegfried(1,"lolcic"));
+        System.out.println(TalkLikeSiegfried.siegfried( 1, "This is KAOS!! We don't play with Kode-Wars here!! " +
+                "So there will be trouble for you this time Mr Maxwell Smart! " +
+                "We have reseived all the photographik evidense we need so choose karefully what you say next! " +
+                "I hope you will ko-operate with KAOS and do exaktly what we want Mr Smart otherwise I won't be happy with you! " +
+                "In fakt, if you misbehave that would make me very unhappy indeed... " +
+                "And you sertainly would not want to make me unnesesarily kross would you Mr Maxwell Smart?"));
     }
 }
--- src/ghozti/codewars/solutions/kyu5/TalkLikeSiegfried.java
+++ src/ghozti/codewars/solutions/kyu5/TalkLikeSiegfried.java
@@ -6,29 +6,55 @@ public class TalkLikeSiegfried {
 
     public static String siegfried(final int week, final String str) {
         String output = "";
-        return week1(str);
+
+        switch (week){
+            case 1:
+                output = week1(str);
+                break;
+            case 2:
+                output = week2(str);
+                break;
+            case 3:
+                output = week3(str);
+                break;
+            case 4:
+                output = week4(str);
+                break;
+            case 5:
+                output = week5(str);
+                break;
+        }
+
+        return output;
     }
 
     private static String week1(String str){
-        String output = "";
+        StringBuilder output = new StringBuilder();
         char[] string_chars = str.toCharArray();
 
         for(int i = 0; i < string_chars.length; i++){
-            if(i  < str.length()-1){
+            if(i  < str.length() - 1){
+
+                boolean action_done = false;
+
                 if(string_chars[i] == 'c'){
                     if(string_chars[i+1] == 'i' || string_chars[i+1] == 'I' || string_chars[i+1] == 'e'|| string_chars[i+1] == 'E'){
                         string_chars[i] = 's';
+                        action_done = true;
                     }
                 }else if(string_chars[i] == 'C'){
                     if(string_chars[i+1] == 'i' || string_chars[i+1] == 'I' || string_chars[i+1] == 'e'|| string_chars[i+1] == 'E'){
                         string_chars[i] = 'S';
+                        action_done = true;
                     }
-                }else if(string_chars[i] == 'c' && string_chars[i+1] != 'h' && string_chars[i+1] != 'H'){
+                }
+
+                if(string_chars[i] == 'c' && string_chars[i+1] != 'h' && string_chars[i+1] != 'H' && !action_done){
                         string_chars[i] = 'k';
-                }else if(string_chars[i] == 'C' && string_chars[i+1] != 'h' && string_chars[i+1] != 'H'){
+                }else if(string_chars[i] == 'C' && string_chars[i+1] != 'h' && string_chars[i+1] != 'H' && !action_done){
                         string_chars[i] = 'K';
                 }
-            }else {
+            }else if(i == str.length() - 1){
                 if(string_chars[i] == 'c'){
                     string_chars[i] = 'k';
                 }else if(string_chars[i] == 'C'){
@@ -36,7 +62,12 @@ public class TalkLikeSiegfried {
                 }
             }
         }
-        return Arrays.toString(string_chars);
+
+        for(char i : string_chars){
+            output.append(i);
+        }
+
+        return output.toString();
     }
 
     private static String week2(String str){
