ba2aab4357ed05e86e7a677b7b8543e044a7747f
Ghozti <62896836+Ghozti@users.noreply.github.com>
2022-04-29 09:35:27 -0400

    - added GreatestWarrior

---
+++ src/ghozti/codewars/solutions/kyu4/GreatestWarrior.java
@@ -0,0 +1,11 @@
+package ghozti.codewars.solutions.kyu4;
+
+public class GreatestWarrior {
+
+    public class Warrior{
+
+        private String[] ranks = new String[] {"Pushover", "Novice", "Fighter", "Warrior", "Veteran", "Sage", "Elite", "Conqueror", "Champion", "Master", "Greatest"};
+        public int level;
+
+    }
+}
